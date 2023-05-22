7d013ba6f6e322c7041da9df5266d9e002cfcd9c
DennisKemboi <cherogonydennis9@gmail.com>
2023-05-22 20:16:16 +0300

    Completed Assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,40 @@
+class Book
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+  
+    def initialize(title)
+      @title = title
+    end
+  
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
+  end
 
+# Where I had reached
+# class Book
+#     attr_accessor(:title, :author, :page_count, :genre, :turn_page)
+#     def initialize(title = "And Then There Were None")
+#         @title = title
+#     end
+    
+#     def author(name = "Agatha Christie")
+#         @name = name
+#     end
 
+#     def page_count(num = 272)
+#         @num = num
+#     end
+
+#     def genre(name = "Mystery")
+#         @name = name
+#     end
+
+#     def turn_page(str = "Flipping the page...wow, you read fast!")
+#         @str = str
+#     end
+# end
+
+# b1 = Book.new
+# b2 = Book.new
+# puts b2.title = "The World According to Garp"
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,43 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
+  
+    def initialize(brand)
+      @brand = brand
+    end
+  
+    def cobble
+      @condition = "new"
+      puts "Your shoe is as good as new!"
+    end
+  end
+
+
+# Where I had reached
+# class Shoe
+#     attr_accessor(:brand, :color, :size, :material, :condition, :cobble)
+#     def initialize(brand = "Adidas")
+#         @brand = brand
+#     end
+
+#     def color(type = "red")
+#         @type = type
+#     end
+
+#     def size(length = 9.5)
+#         @length = length
+#     end
+
+#     def material(type = "suede")
+#         @type = type
+#     end
+
+#     def condition(name = "tattered")
+#         @name = name
+#     end
+
+#     def cobble()
+#         puts "Your shoe is as good as new!"
+#         puts "makes the shoe's condition new"
+#     end
+# end
